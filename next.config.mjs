82b59583c59833f1d765cb868000d20c1f8184ea
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-07-14 23:19:02 +0500

    deploy configs deleted + default build output

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     /* config options here */
     reactCompiler: true,
     reactStrictMode: true,
-    output: 'standalone',
     turbopack: {
         rules: {
             '*.{vert,frag}': {
