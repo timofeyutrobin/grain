466ff8573c3d75744350fce429a3b001cc27f814
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-08-15 21:20:07 +0500

    microscope renderer debounce

--- src/components/editor/Microscope.tsx
+++ src/components/editor/Microscope.tsx
@@ -76,8 +76,14 @@ export const Microscope: React.FC<MicroscopeProps> = ({
             return;
         }
 
-        renderer.render(sampleImage, magnifyGrain(renderParameters));
-    }, [renderParameters, renderer, sampleImage, width, height]);
+        const timer = window.setTimeout(() => {
+            renderer.render(sampleImage, magnifyGrain(renderParameters));
+        }, 100);
+
+        return () => {
+            window.clearTimeout(timer);
+        };
+    }, [renderParameters, renderer, sampleImage]);
 
     return <canvas width={width} height={height} ref={canvasRef} />;
 };
