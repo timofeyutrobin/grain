c9d15b25cdc123ee4bed67e6e65e8a15f61f05d4
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-09-11 21:08:44 +0500

    Clean up WebGL resources by deleting texture after rendering

--- src/lib/rendering/grainRenderer/GrainRenderer.ts
+++ src/lib/rendering/grainRenderer/GrainRenderer.ts
@@ -289,6 +289,7 @@ export class GrainRenderer {
                 );
             }
 
+            this.gl.deleteTexture(imageTexture);
             imageBitmap.close();
             this.gl.flush();
 
