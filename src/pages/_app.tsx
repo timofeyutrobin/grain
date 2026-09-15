fd90266a8b833ccf605e21290aca2b499c4b64ec
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-09-16 02:26:50 +0500

    add favicon and apple-touch-icon links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <link rel="manifest" href="/manifest.json" />
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    href="/icons/icon-medium-maskable.png"
+                />
                 <title>Emulsion Engine</title>
             </Head>
             <Component {...pageProps} />
