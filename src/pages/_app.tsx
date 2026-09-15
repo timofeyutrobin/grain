2d20039b0eacab89493217565c207ca6e3d7f608
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-09-16 02:44:07 +0500

    update favicon and apple-touch-icon links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,21 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <link rel="manifest" href="/manifest.json" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" href="/icons/icon-medium.png" />
                 <link
                     rel="apple-touch-icon"
+                    sizes="152x152"
+                    href="/icons/icon-medium-maskable.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="167x167"
+                    href="/icons/icon-medium-maskable.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
                     href="/icons/icon-medium-maskable.png"
                 />
                 <title>Emulsion Engine</title>
