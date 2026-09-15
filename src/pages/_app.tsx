928263c4422cdf8236538f7a88fd03cb6227a3b1
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2026-09-16 03:01:56 +0500

    180x180 apple-touch-icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,21 +8,10 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-                <link rel="apple-touch-icon" href="/icons/icon-medium.png" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="/icons/icon-medium-maskable.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="167x167"
-                    href="/icons/icon-medium-maskable.png"
-                />
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="/icons/icon-medium-maskable.png"
+                    href="/icons/apple-touch-icon.png"
                 />
                 <title>Emulsion Engine</title>
             </Head>
